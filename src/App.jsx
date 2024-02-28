085eb65610e2a1532daebe315c878012d8a077f1
an0nymID <m.ihsan2131@gmail.com>
2024-02-28 20:02:48 +0800

    feat: add Product Description component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Footer from './components/Footer';
 import ProductDetail from './components/ProductDetail';
 import Header from './components/Header';
+import { ProductDetailDescription } from './components/ProductDetailDescription';
 
 const App = () => {
   return (
-    <>
+    <div>
       <Header />
       <ProductDetail />
+      <ProductDetailDescription/>
       <Footer />
-    </>
+    </div>
   );
 };
 
---
+++ src/components/ProductDetailDescription.jsx
@@ -0,0 +1,9 @@
+export const ProductDetailDescription = () => {
+  return (
+    <div className="max-w-[1060px] m-auto">
+      <div>
+        ihsan tes tes
+      </div>
+    </div>
+  )
+}
